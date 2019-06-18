0e0ed36662324021d026e094818eb061cd794093
Jaque <45607757+japerez107@users.noreply.github.com>
2019-06-18 00:00:28 -0400

    updating friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,16 +4,16 @@ var friendArray = [
     "name":"Mary",
     "photo":"https://image.shutterstock.com/image-photo/surprised-winking-model-girl-holding-450w-464960153.jpg",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
+        '5',
+        '1',
+        '4',
+        '4',
+        '5',
+        '1',
+        '2',
+        '5',
+        '4',
+        '1'
       ]
   }
 
