6ecb1d6aa19c01e793b403781b658bc85315faab
Jaque <45607757+japerez107@users.noreply.github.com>
2019-06-17 20:19:19 -0400

    big update

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+
+var friendArray = [
+{
+    "name":"Mary",
+    "photo":"https://image.shutterstock.com/image-photo/surprised-winking-model-girl-holding-450w-464960153.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+
+]
+
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,34 @@
+var friendData = require("../data/friends");
+
+module.exports = function(app) {
+    // API GET Requests
+   
+    app.get("/api/survey", function(req, res) {
+      res.json(friendData);
+    });
+  
+  
+    // API POST Requests
+    
+    app.post("/api/survey", function(req, res) {
+      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+      // It will do this by sending out the value "true" have a table
+      // req.body is available since we're using the body parsing middleware
+      if (friendData.length < 5) {
+        friendData.push(req.body);
+        res.json(true);
+      }
+    });
+  
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+  
+    app.post("/api/clear", function(req, res) {
+      // Empty out the arrays of data
+      friendData.length = [];
+
+      res.json({ ok: true });
+    });
+  };
+  
