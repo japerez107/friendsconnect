4b12449463e15eb5b139895b3311fd0d4586adf4
Jaque <45607757+japerez107@users.noreply.github.com>
2019-06-17 23:32:19 -0400

    updating survey questions

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,24 +11,19 @@ module.exports = function(app) {
     // API POST Requests
     
     app.post("/api/survey", function(req, res) {
-      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-      // It will do this by sending out the value "true" have a table
-      // req.body is available since we're using the body parsing middleware
-      if (friendData.length < 5) {
+
+      var user = req.body;
+
+      if (friendData.length < ) {
         friendData.push(req.body);
         res.json(true);
       }
-    });
-  
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-  
-    app.post("/api/clear", function(req, res) {
-      // Empty out the arrays of data
-      friendData.length = [];
 
-      res.json({ ok: true });
+
+      friendData.push(user);
     });
+    
+  
+
   };
   
