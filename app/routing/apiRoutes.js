2045df3983b4b91e9af59f603db00567baf04f60
Jaque <45607757+japerez107@users.noreply.github.com>
2019-06-17 23:33:32 -0400

    updating api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,26 @@
 var friendData = require("../data/friends");
 
-module.exports = function(app) {
-    // API GET Requests
-   
-    app.get("/api/survey", function(req, res) {
-      res.json(friendData);
-    });
-  
-  
-    // API POST Requests
-    
-    app.post("/api/survey", function(req, res) {
+module.exports = function (app) {
+  // API GET Requests
 
-      var user = req.body;
-
-      if (friendData.length < ) {
-        friendData.push(req.body);
-        res.json(true);
-      }
+  app.get("/api/survey", function (req, res) {
+    res.json(friendData);
+  });
 
 
-      friendData.push(user);
-    });
-    
-  
+  // API POST Requests
 
-  };
-  
+  app.post("/api/survey", function (req, res) {
+
+    var user = req.body;
+
+    if (friendData.length < 5) {
+      friendData.push(req.body);
+      res.json(true);
+    }
+
+    //pushes new data to api 
+    friendData.push(user);
+  });
+
+};
